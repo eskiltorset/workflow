4a808577b967ca75abcb8679727d99600a036ec8
eskiltorset <44784426+eskiltorset@users.noreply.github.com>
2023-11-06 14:39:05 +0100

    test for automatic prettier and eslint

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import router from "./router/index.js";
 import ui from "./ui/index.js";
 
-ui()
-router()
+ui();
+router();
 
-console . log  
-
-
-(   "Intentional Formatting Error"   )  ;
+console.log  
+    ( "Intentional Error" );;;
